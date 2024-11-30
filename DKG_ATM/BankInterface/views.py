4cc577dc0786bcc5abe6b46b4aa4ae290e048394
Abhishek_R <128019912+Abhi3468@users.noreply.github.com>
2024-11-30 10:07:48 +0530

    Updated

--- DKG_ATM/BankInterface/views.py
+++ DKG_ATM/BankInterface/views.py
@@ -7,44 +7,44 @@ from decimal import Decimal
 import json
 from .models import Account, WithdrawalHistory, DepositHistory
 
-@method_decorator(csrf_exempt, name='dispatch')
-class AccountListView(View):
-    def get(self, request):
-        accounts = Account.objects.all().values()
-        return JsonResponse(list(accounts), safe=False)
+# @method_decorator(csrf_exempt, name='dispatch')
+# class AccountListView(View):
+#     def get(self, request):
+#         accounts = Account.objects.all().values()
+#         return JsonResponse(list(accounts), safe=False)
     
-    def post(self, request):
-        data = json.loads(request.body)
-        account = Account.objects.create(
-            card_number=data['card_number'],
-            card_pin=data['card_pin'],
-            balance=data['balance']
-        )
-        return JsonResponse({'card_number': account.card_number}, status=201)
+#     def post(self, request):
+#         data = json.loads(request.body)
+#         account = Account.objects.create(
+#             card_number=data['card_number'],
+#             card_pin=data['card_pin'],
+#             balance=data['balance']
+#         )
+#         return JsonResponse({'card_number': account.card_number}, status=201)
 
-@method_decorator(csrf_exempt, name='dispatch')
-class AccountDetailView(View):
-    def get(self, request, card_number):
-        account = get_object_or_404(Account, card_number=card_number)
-        return JsonResponse({
-            'card_number': account.card_number,
-            'card_pin': account.card_pin,
-            'balance': account.balance
-        })
+# @method_decorator(csrf_exempt, name='dispatch')
+# class AccountDetailView(View):
+#     def get(self, request, card_number):
+#         account = get_object_or_404(Account, card_number=card_number)
+#         return JsonResponse({
+#             'card_number': account.card_number,
+#             'card_pin': account.card_pin,
+#             'balance': account.balance
+#         })
 
-    def put(self, request, card_number):
-        data = json.loads(request.body)
-        account = get_object_or_404(Account,card_number=card_number)
-        account.card_number = data.get('card_number', account.card_number)
-        account.card_pin = data.get('card_pin', account.card_pin)
-        account.balance = data.get('balance', account.balance)
-        account.save()
-        return JsonResponse({'card_number': account.card_number})
+#     def put(self, request, card_number):
+#         data = json.loads(request.body)
+#         account = get_object_or_404(Account,card_number=card_number)
+#         account.card_number = data.get('card_number', account.card_number)
+#         account.card_pin = data.get('card_pin', account.card_pin)
+#         account.balance = data.get('balance', account.balance)
+#         account.save()
+#         return JsonResponse({'card_number': account.card_number})
 
-    def delete(self, request, card_number):
-        account = get_object_or_404(Account, card_number=card_number)
-        account.delete()
-        return JsonResponse({'message': 'Account deleted successfully'}, status=204)
+#     def delete(self, request, card_number):
+#         account = get_object_or_404(Account, card_number=card_number)
+#         account.delete()
+#         return JsonResponse({'message': 'Account deleted successfully'}, status=204)
 
 @method_decorator(csrf_exempt, name='dispatch')
 class WithdrawalView(View):
